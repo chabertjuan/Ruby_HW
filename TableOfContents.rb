626b725728bc309e4f960c8b892dc26adf6fbc1c
Juan Chabert <juanchabertmac@Juans-MacBook-Pro.local>
2015-10-22 17:30:14 -0400

    First Ruby HW

---
+++ TableOfContents.rb
@@ -0,0 +1,6 @@
+line_width = 60
+puts "Table of Contents".center(line_width) + "\n\n"
+puts ("=" * line_width) + "\n\n"
+puts "Chapter 1: Basic Telemetry".ljust(line_width/2) + "Page 1".rjust(line_width/2)
+puts "Chapter 2: Metering".ljust(line_width/2) + "Page 4".rjust(line_width/2)
+puts "Chapter 3: SCADA".ljust(line_width/2) + "Page 9".rjust(line_width/2)
